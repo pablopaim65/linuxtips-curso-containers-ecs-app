d99c1138148750d149d7717bcb9954b124247511
PAblo <pablopaim65@gmail.com>
2024-11-05 22:45:58 -0300

    modulo v4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "service" {
-  source = "github.com/pablopaim65/linuxtips-curso-containers-ecs-service-module?ref=v1.3.0"
+  source = "github.com/pablopaim65/linuxtips-curso-containers-ecs-service-module?ref=v1.4.0"
   #source = "/home/pablo/linuxtips-curso-containers-ecs-service-module"
   #source = "github.com/pablopaim65/linuxtips-curso-containers-ecs-service-module?ref=v1.1.0"
   #source = "github.com/pablopaim65/linuxtips-curso-containers-ecs-service-module?ref=v1.2.1"
